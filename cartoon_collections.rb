d0659ff7757da32d3553795207d76da02fd93a58
Lisa LaRochelle <larochelle.lisa@gmail.com>
2019-07-09 17:59:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,8 @@ def long_planeteer_calls(array)
   array.any? { |words| words.length > 4}
 end
 
-def find_the_cheese(array)
+def find_the_cheese(cheese_array, ingredients_array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  
 end
