6d5318164e94d97c791423f128e7e88b59da2ee4
Lisa LaRochelle <larochelle.lisa@gmail.com>
2019-07-09 17:12:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(array)
-  array.each_with_index do |index, name|
-        puts "#{index}. #{name}"
+  array.each_with_index do |name, index|
+        puts "#{index} #{name}"
   end
 end
 
