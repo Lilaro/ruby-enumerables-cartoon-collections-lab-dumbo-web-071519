1f2229daf970e101de4a99fde5f7e5510c02f01c
Lisa LaRochelle <larochelle.lisa@gmail.com>
2019-07-09 17:41:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.each do |element|
+  array.any? do |element|
    if element.length > 4
      return true
   else return false
