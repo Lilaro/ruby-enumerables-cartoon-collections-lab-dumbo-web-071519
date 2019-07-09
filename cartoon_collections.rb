49b57d11797050e567b0146a5d5a3205efa0ed64
Lisa LaRochelle <larochelle.lisa@gmail.com>
2019-07-09 17:52:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,8 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.any? { |words| words.length > 4
+  array.any? { |words| words.length > 4}
+end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
